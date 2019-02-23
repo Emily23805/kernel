328b3d410081e5900e338ae8266b364840c71883
Emily23805 <33092539+Emily23805@users.noreply.github.com>
2019-02-24 01:49:00 +0900

    kernel-downloader

---
+++ download.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "kernel verson example : 4.x or 1.1"
+echo "kernel verson"
+read KV
+echo "kernel Advanced verson example : 4.1.13"
+echo "kernel Advanced verson"
+read KAV
+wget https://mirrors.edge.kernel.org/pub/linux/kernel/v$KV/linux-$KAV.tar.gz
+#This site is newell-server open mirror url
+#
+#
+#
+#wget http://newell.kro.kr/linux/kernel/v$KV/linux-$KAV.tar.gz
